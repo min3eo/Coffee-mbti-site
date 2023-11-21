d729224e38122ae0839ed94eb1f32deea519b082
min3eo <kane7674@naver.comm>
2023-11-21 21:16:12 +0900

    초기 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from 'pages/Main';
+import Test from 'pages/Test';
+import Result from 'pages/Result';
+import View from 'pages/View';
+import NotFound from 'pages/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />}/>
+          <Route path='/test' element={<Test />}/>
+          <Route path='/result' element={<Result />}/>
+          <Route path='/view' element={<View />}/>
 
-export default App;
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/not-found" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from 'pages/Main';
+import Test from 'pages/Test';
+import Result from 'pages/Result';
+import View from 'pages/View';
+import NotFound from 'pages/NotFound';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />}/>
+          <Route path='/test' element={<Test />}/>
+          <Route path='/result' element={<Result />}/>
+          <Route path='/view' element={<View />}/>
+
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/not-found" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App;
